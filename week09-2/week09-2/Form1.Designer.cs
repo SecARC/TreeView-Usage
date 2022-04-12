041f0961fc0a06804364cacd80ec0129e6b010d3
Tuğra GÜNDÜZ <trgndz1999@gmail.com>
2022-04-12 18:55:09 +0300

    TreeView Usage

---
+++ week09-2/week09-2/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿
+namespace week09_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Car", 0, 0);
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Bicycle", 1, 1);
+            System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Truck", 4, 4);
+            System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Vehicles", 5, 5, new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2,
+            treeNode3});
+            System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Hotel", 3, 3);
+            System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("House Share", 2, 2);
+            System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Accomodation", 6, 6, new System.Windows.Forms.TreeNode[] {
+            treeNode5,
+            treeNode6});
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.ListViewItem listViewItem1 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Citroen C4",
+            "2009",
+            "150.000 km"}, -1);
+            System.Windows.Forms.ListViewItem listViewItem2 = new System.Windows.Forms.ListViewItem(new string[] {
+            "Renault Megane",
+            "2021",
+            "0 km"}, -1);
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.treeView2 = new System.Windows.Forms.TreeView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.treeView3 = new System.Windows.Forms.TreeView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.ImageIndex = 0;
+            this.treeView1.ImageList = this.ımageList1;
+            this.treeView1.Location = new System.Drawing.Point(13, 13);
+            this.treeView1.Name = "treeView1";
+            treeNode1.ImageIndex = 0;
+            treeNode1.Name = "Düğüm1";
+            treeNode1.SelectedImageIndex = 0;
+            treeNode1.Text = "Car";
+            treeNode2.ImageIndex = 1;
+            treeNode2.Name = "Düğüm3";
+            treeNode2.SelectedImageIndex = 1;
+            treeNode2.Text = "Bicycle";
+            treeNode3.ImageIndex = 4;
+            treeNode3.Name = "Düğüm4";
+            treeNode3.SelectedImageIndex = 4;
+            treeNode3.Text = "Truck";
+            treeNode4.ImageIndex = 5;
+            treeNode4.Name = "Düğüm0";
+            treeNode4.SelectedImageIndex = 5;
+            treeNode4.Text = "Vehicles";
+            treeNode5.ImageIndex = 3;
+            treeNode5.Name = "Düğüm8";
+            treeNode5.SelectedImageIndex = 3;
+            treeNode5.Text = "Hotel";
+            treeNode6.ImageIndex = 2;
+            treeNode6.Name = "Düğüm9";
+            treeNode6.SelectedImageIndex = 2;
+            treeNode6.Text = "House Share";
+            treeNode7.ImageIndex = 6;
+            treeNode7.Name = "Düğüm7";
+            treeNode7.SelectedImageIndex = 6;
+            treeNode7.Text = "Accomodation";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode7});
+            this.treeView1.SelectedImageIndex = 0;
+            this.treeView1.Size = new System.Drawing.Size(136, 425);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "iconfinder_aiga_taxi_134116.png");
+            this.ımageList1.Images.SetKeyName(1, "iconfinder_bicycle_172573.png");
+            this.ımageList1.Images.SetKeyName(2, "iconfinder_house-home-main-menu-start-building_3643769.png");
+            this.ımageList1.Images.SetKeyName(3, "iconfinder_ic_local_hotel_48px_352502.png");
+            this.ımageList1.Images.SetKeyName(4, "iconfinder_truck_115775.png");
+            this.ımageList1.Images.SetKeyName(5, "iconfinder_Steering_wheel_2501882.png");
+            this.ımageList1.Images.SetKeyName(6, "iconfinder_smugmug_334755.png");
+            // 
+            // treeView2
+            // 
+            this.treeView2.ImageIndex = 0;
+            this.treeView2.ImageList = this.ımageList1;
+            this.treeView2.Location = new System.Drawing.Point(155, 42);
+            this.treeView2.Name = "treeView2";
+            this.treeView2.SelectedImageIndex = 0;
+            this.treeView2.Size = new System.Drawing.Size(136, 328);
+            this.treeView2.TabIndex = 1;
+            this.treeView2.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterSelect);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(136, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Create TreeNodes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(298, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Selected Node";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(301, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(343, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 373);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "New Node";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(155, 389);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(136, 20);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(155, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Add Child Node";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // treeView3
+            // 
+            this.treeView3.Location = new System.Drawing.Point(301, 126);
+            this.treeView3.Name = "treeView3";
+            this.treeView3.Size = new System.Drawing.Size(343, 312);
+            this.treeView3.TabIndex = 8;
+            // 
+            // textBox3
+            // 
+            this.textBox3.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox3.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBox3.Location = new System.Drawing.Point(301, 95);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(569, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Browse";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox4.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBox4.Location = new System.Drawing.Point(662, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(92, 20);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox5.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBox5.Location = new System.Drawing.Point(760, 95);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(92, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(956, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Add";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.FullRowSelect = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Items.AddRange(new System.Windows.Forms.ListViewItem[] {
+            listViewItem1,
+            listViewItem2});
+            this.listView1.Location = new System.Drawing.Point(662, 126);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(369, 312);
+            this.listView1.TabIndex = 14;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Brand";
+            this.columnHeader1.Width = 122;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Model Year";
+            this.columnHeader2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader2.Width = 72;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "KM";
+            this.columnHeader3.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.columnHeader3.Width = 88;
+            // 
+            // textBox6
+            // 
+            this.textBox6.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.SuggestAppend;
+            this.textBox6.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.textBox6.Location = new System.Drawing.Point(858, 95);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(92, 20);
+            this.textBox6.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1039, 450);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.treeView3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.treeView2);
+            this.Controls.Add(this.treeView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.TreeView treeView2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TreeView treeView3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ week09-2/week09-2/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace week09_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            treeView2.Nodes.Clear();
+
+            TreeNode root = new TreeNode("Vehicle");
+            root.ImageIndex = 5;
+            root.SelectedImageIndex = 5;
+
+            TreeNode node = new TreeNode("Car");
+            node.ImageIndex = 0;
+            node.SelectedImageIndex = 0;
+            root.Nodes.Add(node);
+
+            node = new TreeNode("Bicycle");
+            node.ImageIndex = 1;
+            node.SelectedImageIndex = 1;
+            root.Nodes.Add(node);
+
+
+            treeView2.Nodes.Add(root);
+        }
+
+        private void treeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            TreeView view = sender as TreeView;
+            textBox1.Text = view.SelectedNode.FullPath;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            TreeNode parent = treeView2.SelectedNode;
+            if (parent != null)
+            {
+                TreeNode node = new TreeNode(textBox2.Text);
+                node.ImageIndex = parent.ImageIndex;
+                node.SelectedImageIndex = parent.SelectedImageIndex;
+                parent.Nodes.Add(node);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(textBox3.Text))
+            {
+                TreeNode root = treeView3.Nodes.Add(textBox3.Text);
+                populateDirectoryTree(textBox3.Text, root);
+            }
+        }
+
+        private void populateDirectoryTree(string path, TreeNode parent)
+        {
+            try
+            {
+                string[] dirNames = Directory.GetDirectories(path);
+                if (dirNames.Count() == 0)
+                    return;
+
+                
+
+                foreach (var dir in dirNames)
+                {
+                    TreeNode node = parent.Nodes.Add(Path.GetFileNameWithoutExtension(dir));
+                    populateDirectoryTree(dir, node);
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                parent.Nodes.Add("Access Denied");
+            }
+            catch (Exception)
+            {
+                parent.Nodes.Add("Error!");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            var item = listView1.Items.Add(textBox4.Text);
+            item.SubItems.Add(textBox5.Text);
+            item.SubItems.Add(textBox6.Text);
+        }
+    }
+}
